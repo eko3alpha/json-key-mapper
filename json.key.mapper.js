cede3a113e07724be67579cbec330d4931a70577
Angel Alvarado <eko3alpha@gmail.com>
2015-09-18 09:49:26 -0400

    Initial Commit
    Tests
    Library

---
+++ json.key.mapper.js
@@ -0,0 +1,140 @@
+/* globals define */
+'use strict';
+(function(root, factory){
+    if(typeof define === 'function' && define.amd){
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else{
+        // Browser globals
+        root.jkm = factory();
+    }
+}(this, function(){
+
+    /**
+     * Holds configuration
+     * @type {Object}
+     */
+
+    var config = {map: []};
+
+    /**
+     * Stores a mapping profile with a given id
+     * @param {string} name   mapping profile name
+     * @param {object} newMap json object defining your mapping
+     */
+
+    var setMap = function(name, newMap){
+        config.map[name] = newMap;
+    };
+
+    /**
+     * Retrieves the mapping profile for a given name
+     * @param  {string} name mapping profile name
+     * @return {object}      mapping profile
+     */
+
+    var getMap = function(name){
+        if(config.map[name]){
+            return config.map[name];
+        }
+        return {};
+    };
+
+    /**
+     * Returns the mapped data based on mapping name
+     * @param  {string} mapName mapping profile name
+     * @param  {object|array} data    data to be converted
+     * @return {object|array}         mapped data
+     */
+
+    var map = function(mapName, data){
+
+        // if no data is passed in then exit, nothing to map
+        if(data === undefined){
+            return;
+        }
+
+        var map = getMap(mapName);
+
+        // if map is an empty object then
+        // return value as is
+
+        if(Object.keys(map).length === 0){
+            return data;
+        }
+
+        // if data passed is not an array then its
+        // a single object so use mapItem
+        if(data.length === undefined){
+            return mapItem(data, map);
+        }
+
+        // if data passed is an array then
+        // use mapArray to map the data
+        return mapArray(data, map);
+    };
+
+    /**
+     * Maps arrays based on mapping provided
+     * @param  {array} myArray array of json objects
+     * @param  {object} map  mapping profile
+     * @return {array}       array containing mapped elements
+     */
+
+    var mapArray = function(myArray, map){
+        var mappedArray = [];
+        for(var i = 0; i < myArray.length; i++){
+            mappedArray.push(mapItem(myArray[i], map));
+        }
+        return mappedArray;
+    };
+
+    /**
+     * Swaps key and values
+     * Currently not being used ( planning for future update )
+     * @param  {object} json json to be swapped
+     * @return {object}      json whose property and values have been switched
+     */
+
+    var swapPropVals = function(json){
+        var swapped = {};
+        for(var key in json){
+            if(json.hasOwnProperty(key)){
+                swapped[json[key]] = key;
+            }
+        }
+        return swapped;
+    };
+
+    /**
+     * Maps json based on mapping provided keeping
+     * unmapped properties untouched
+     * @param  {array} item  json object
+     * @param  {object} map  mapping profile
+     * @return {object}       mapped json object
+     */
+
+    var mapItem = function(item, map){
+        for(var prop in map){
+            if(map.hasOwnProperty(prop)){
+                item[map[prop]] = item[prop];
+                delete item[prop];
+            }
+        }
+        return item;
+    };
+
+    /**
+     * returns interface
+     */
+
+    return {
+            __private: {config,
+                        getMap: getMap,
+                        mapItem: mapItem,
+                        mapArray: mapArray},
+            setMap: setMap,
+            swapPropVals: swapPropVals,
+            map: map
+        };
+}));
---
+++ tests/tests.js
@@ -0,0 +1,138 @@
+/* globals jkm, QUnit */
+'use strict';
+
+QUnit.test('+ PUBLIC (setter / getter)', function(assert){
+
+    var mapA = {a: 'b'};
+    var mapB = {z: 'd'};
+
+    jkm.setMap('testA', {a: 'b'});
+    jkm.setMap('testB', {z: 'd'});
+
+    assert.propEqual(jkm.__private.getMap('testA'), mapA, 'getMap()');
+    assert.propEqual(jkm.__private.getMap('testB'), mapB, 'getMap()');
+
+});
+
+QUnit.test('+ PUBLIC (swap properties and values)', function(assert){
+
+    var testData = {
+                    id: 'mapId',
+                    school_id: 'schoolId',
+                    name: 'positionName'
+                };
+
+    var testCase = {
+                    mapId: 'id',
+                    schoolId: 'school_id',
+                    positionName: 'name'
+                };
+
+    assert.propEqual(jkm.swapPropVals(testData), testCase, 'swapKeys()');
+});
+
+QUnit.test('+ PUBLIC (mapping implimentation)', function(assert){
+
+    var testData, testCase;
+
+    //------------------------------------------------------------------------------------
+
+    jkm.setMap('test', {a: 'b'});
+    testData = {a: 100};
+    testCase = {b: 100};
+    assert.propEqual(jkm.map('test', testData), testCase, 'map() item');
+
+    //------------------------------------------------------------------------------------
+
+    jkm.setMap('test', {a: 'b'});
+    testData = [{a: 100}];
+    testCase = [{b: 100}];
+    assert.propEqual(jkm.map('test', testData), testCase, 'map() array');
+
+    //------------------------------------------------------------------------------------
+
+    jkm.setMap('test', {});
+    testData = [{a: 100}];
+    testCase = [{a: 100}];
+    assert.propEqual(jkm.map('test', testData), testCase, 'map() with no mapping should return same object');
+
+    //------------------------------------------------------------------------------------
+
+    jkm.setMap('no name', {a: 'b'});
+    testData = {a: 100};
+    testCase = {a: 100};
+    assert.propEqual(jkm.map('test', testData), testCase, 'map() item with no mapping should return the same object');
+
+    //------------------------------------------------------------------------------------
+
+    jkm.setMap('oneItem', {a: 'z'});
+    testData = {a: 100, b: 200};
+    testCase = {z: 100, b: 200};
+    assert.propEqual(jkm.map('oneItem', testData), testCase, 'map() only one property, keep the rest');
+
+    //------------------------------------------------------------------------------------
+
+    jkm.setMap('conflictedMapping', {a: 'z'});
+    testData = {a: 100, b: 200, z: 300};
+    testCase = {z: 100, b: 200};
+    assert.propEqual(jkm.map('conflictedMapping', testData), testCase, 'map() remove existing property if it conflicts with mapped version');
+});
+
+QUnit.test('- PRIVATE (single object mapping)', function(assert){
+
+    var testMap = {
+                    id: 'mapId',
+                    school_id: 'schoolId',
+                    name: 'positionName'
+                };
+
+    jkm.setMap('test', testMap);
+
+    var testData = {
+                    id: 12,
+                    school_id: 'A',
+                    name: 'test name'
+                };
+
+    var testCase = {
+                    mapId: 12,
+                    schoolId: 'A',
+                    positionName: 'test name'
+                };
+
+    assert.propEqual(jkm.__private.mapItem(testData, testMap), testCase, 'mapItem()');
+
+});
+
+QUnit.test('- PRIVATE (array object mapping)', function(assert){
+
+    var testMap = {
+                    id: 'mapId',
+                    school_id: 'schoolId',
+                    name: 'positionName'
+                };
+
+    var testData = [{
+                    id: 12,
+                    school_id: 'A',
+                    name: 'test name'
+                },
+                {
+                    id: 15,
+                    school_id: 'B',
+                    name: 'blah name'
+                }];
+
+    var testCase = [{
+                    mapId: 12,
+                    schoolId: 'A',
+                    positionName: 'test name'
+                },{
+                    mapId: 15,
+                    schoolId: 'B',
+                    positionName: 'blah name'
+                }];
+
+    assert.propEqual(jkm.__private.mapArray(testData, testMap), testCase, 'mapArray()');
+
+});
